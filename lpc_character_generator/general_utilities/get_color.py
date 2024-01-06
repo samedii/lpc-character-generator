2bd39d69171548361b330ca5fef751acf06733f8
Zuka Chilachava <zchil19@freeuni.edu.ge>
2024-01-06 23:08:32 +0400

    changing global random seed to a local one

--- lpc_character_generator/general_utilities/get_color.py
+++ lpc_character_generator/general_utilities/get_color.py
@@ -2,7 +2,7 @@ import random
 
 
 def get_color(seed):
-    random.seed(seed)
-    random_color = "#{:06x}".format(random.randint(0, 0xFFFFFF))
+    local_random = random.Random(seed)
+    random_color = "#{:06x}".format(local_random.randint(0, 0xFFFFFF))
 
     return random_color
