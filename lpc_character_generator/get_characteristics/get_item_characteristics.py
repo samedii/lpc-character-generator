c876c05fde0f57273c315a3c6aa91053bfbade95
Zuka Chilachava <zchil19@freeuni.edu.ge>
2024-02-08 15:06:32 +0400

    convert colors to more appropriate names for descriptions

--- lpc_character_generator/get_characteristics/get_item_characteristics.py
+++ lpc_character_generator/get_characteristics/get_item_characteristics.py
@@ -1,7 +1,16 @@
+import json
+
+from lpc_character_generator.constants import PATH_TO_COLOR_NAMES
+
+
 def get_item_characteristics(item_name: str) -> dict:
+    color_dict = json.load(open(PATH_TO_COLOR_NAMES))
     split_value = item_name.split()
+    custom_color = split_value[-1]
+    converted_color = color_dict.get(custom_color, custom_color)
+
     characteristics = {
-        "color": split_value[-1],
+        "color": converted_color,
         "name": " ".join(split_value[:-1]),
     }
 
