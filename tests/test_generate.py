9b0b46a6006774f734f2c896c7711d38feb2c8e6
Zuka Chilachava <zchil19@freeuni.edu.ge>
2024-03-30 18:17:37 +0400

    updated generate test

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -4,18 +4,24 @@ import numpy as np
 import lpc_character_generator as generator
 
 from PIL import Image
-from lpc_character_generator.constants import Action, Direction, ALLOWED_DIRECTIONS, NON_ROTATION_ACTIONS
+from lpc_character_generator.constants import (
+    Action,
+    Direction,
+    ALLOWED_DIRECTIONS,
+    NON_ROTATION_ACTIONS,
+)
 
 _ROTATION_ACTIONS = set(Action) - NON_ROTATION_ACTIONS
 
 
 def run_test(inputs, prefix):
     generated_characters = []
+    for_rotation_group = inputs.get("for_rotation_groups", False)
 
     start_time = time.time()
     n_times = 10
 
-    for i in range(n_times):
+    for _ in range(n_times):
         generated_character = generator.generate(**inputs)
 
         animation = generated_character["animation"]
@@ -24,10 +30,11 @@ def run_test(inputs, prefix):
         settings = generated_character["settings"]
 
         for param_name, param_value in inputs.items():
-            assert settings[param_name] == param_value
+            if param_name != "for_rotation_groups":
+                assert settings[param_name] == param_value
 
-        if settings['is_rotation']:
-            assert generated_character.get('rotation_groups', None) is not None
+        if for_rotation_group:
+            assert generated_character.get("rotation_groups", None) is not None
 
         assert isinstance(animation[0], (Image.Image, np.ndarray))
 
@@ -35,9 +42,7 @@ def run_test(inputs, prefix):
 
     for index, images in enumerate(generated_characters):
         animation = Image.fromarray(np.concatenate(images, axis=1))
-        animation.save(
-            f"tests/results/{prefix}_{index}.png"
-        )
+        animation.save(f"tests/results/{prefix}_{index}.png")
 
     assert (end_time - start_time) / n_times <= 0.1
 
@@ -49,10 +54,7 @@ def test_full_random():
 @pytest.mark.parametrize("direction", Direction)
 @pytest.mark.parametrize("action", Action)
 def test_direction(action, direction):
-    inputs = {
-        "action": action,
-        "direction": direction
-    }
+    inputs = {"action": action, "direction": direction}
     available_dirs = ALLOWED_DIRECTIONS.get(action, set(Direction))
 
     if direction not in available_dirs:
@@ -63,9 +65,12 @@ def test_direction(action, direction):
 
 @pytest.mark.parametrize("action", _ROTATION_ACTIONS)
 def test_rotation(action):
-    inputs = {
-        "action": action,
-        "is_rotation": True
-    }
+    inputs = {"action": action, "is_rotation": True}
 
     run_test(inputs, f"{action}_rotation")
+
+
+def test_for_rotation_groups():
+    inputs = {"is_rotation": True, "for_rotation_groups": True}
+
+    run_test(inputs, f"rotation_groups")
